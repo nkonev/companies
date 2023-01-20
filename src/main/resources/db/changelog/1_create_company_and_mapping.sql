732e9367b2aa7ffebf0e0d86298bb7695565eb31
Nikita Konev <nikit.cpp@yandex.ru>
2023-01-20 19:21:16 +0400

    App starts

--- src/main/resources/db/changelog/1_create_company_and_mapping.sql
+++ src/main/resources/db/changelog/1_create_company_and_mapping.sql
@@ -1,49 +1,15 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- get drafts = get commits ? by companyId
--- get transactions = get branches by userId
-
--- like git branch
-CREATE TABLE branch(
-    id uuid PRIMARY KEY default uuid_generate_v4(),
-    name varchar(255),
-    owner_id uuid -- null means 'system branch', for example 'main'
-);
-
-create table commits(
-    id UUID primary key default uuid_generate_v4(),
-    branch_id uuid REFERENCES branch(id) ON DELETE CASCADE
-);
-
-CREATE TABLE bank(
-    id uuid PRIMARY KEY default uuid_generate_v4(),
-    name varchar(255),
-    bank_account varchar(255),
-    estimated_size int,
-
-    -- git fields
-    modified_at TIMESTAMP,
-    message
-);
-
 CREATE TABLE company(
-    id uuid PRIMARY KEY default uuid_generate_v4(),
-    name varchar(255),
-    bank_account varchar(255),
-    estimated_size int,
-    bank_id REFERENCES bank(id),
-
-    -- git fields
-    modified_at TIMESTAMP,
-    message   text
+                        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+                        name varchar(255),
+                        bank_account varchar(255),
+                        estimated_size int,
+                        modified_at TIMESTAMP
 );
 
-
-CREATE TABLE company_branch(
-    id uuid PRIMARY KEY default uuid_generate_v4(),
-    branch_id uuid REFERENCES branch(id) ON DELETE CASCADE,
-    company_id uuid REFERENCES company_snapshots(id) ON DELETE CASCADE
+CREATE TABLE mapping(
+                        branch_id UUID PRIMARY KEY,
+                        user_id UUID NOT NULL,
+                        company_id UUID NOT NULL
 );
-
-insert into branch(id, name) values
-    ('00000000-0000-0000-0000-000000000000', 'main');
--- src/main/resources/db/changelog/2_add_data.sql
+++ src/main/resources/db/changelog/2_add_data.sql
@@ -1,9 +1,9 @@
 -- https://dev.mysql.com/blog-archive/mysql-8-0-uuid-support/
 INSERT INTO company(id, name)
 VALUES
-    (UUID_TO_BIN(uuid()), 'P-Company');
+    (uuid_generate_v4(), 'P-Company');
 
 INSERT INTO company(id, name)
 VALUES
-    (unhex(replace(uuid(),'-','')), 'O-Company');
+    (uuid_generate_v4(), 'O-Company');
 
