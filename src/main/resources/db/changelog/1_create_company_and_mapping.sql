ba32655c75913ce9eab2dddcd24f0a2230f34625
Nikita Konev <nikit.cpp@yandex.ru>
2023-01-20 12:30:57 +0400

    Initial commit with postgres

--- src/main/resources/db/changelog/1_create_company_and_mapping.sql
+++ src/main/resources/db/changelog/1_create_company_and_mapping.sql
@@ -1,18 +1,29 @@
-CALL DOLT_CHECKOUT('main');
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE company(
-    id binary(16) DEFAULT (UUID_TO_BIN(uuid())) PRIMARY KEY,
+-- like git branch
+CREATE TABLE branch(
+    id uuid PRIMARY KEY default uuid_generate_v4(),
+    name varchar(255),
+    owner_id uuid -- null means 'system branch', for example 'main'
+);
+
+CREATE TABLE company_snapshots(
+    id uuid PRIMARY KEY default uuid_generate_v4(),
     name varchar(255),
     bank_account varchar(255),
     estimated_size int,
-    modified_at TIMESTAMP
+
+    -- git fields
+    modified_at TIMESTAMP,
+    branch_id uuid REFERENCES branch(id),
+    message   text not null
 );
 
-CREATE TABLE mapping(
-    branch_id binary(16) PRIMARY KEY,
-    user_id binary(16) NOT NULL,
-    company_id binary(16) NOT NULL
+CREATE TABLE company_branch(
+    id uuid PRIMARY KEY default uuid_generate_v4(),
+    branch_id uuid REFERENCES branch(id) ON DELETE CASCADE,
+    company_id uuid REFERENCES company(id) ON DELETE CASCADE
 );
 
-CALL DOLT_ADD('.');
-CALL DOLT_COMMIT('-m', 'Company and mapping is created', '--author', 'Liquibase Migrations <liquibase@example.com>');
+insert into branch(id, name) values
+    ('00000000-0000-0000-0000-000000000000', 'main');
