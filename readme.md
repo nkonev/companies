5a4a07eb4f6f8fccf42bd67988c24fa18395c67d
Nikita Konev <nikit.cpp@yandex.ru>
2023-01-20 18:45:20 +0400

    Add loading repro

--- readme.md
+++ readme.md
@@ -14,8 +14,68 @@ curl -Ss -H 'X-Userid: c092a0ad-8148-4291-9194-6c3d12a1120a' -H 'Content-Type: a
 
 ```
 
-# Load test
+# Load testing
+
+## 50000 branches
 ```
 docker-compose up -d
-./mvnw -DenableLoadTests=true -Dtest="name.nkonev.examples.companies.VolumeTest#fifty_thousands_branches" test
+./mvnw -DenableLoadTests=true -Dtest="name.nkonev.examples.companies.VolumeTest#fifty_thousand_branches" test
+```
+
+
+## Create 7000 companies, then add a commit to every company three times
+### Run web app in the first terminal window
+```
+./mvnw spring-boot:run
+```
+
+### Then create 7000 companies th another terminal
+```
+./mvnw -DenableLoadTests=true -Dtest="name.nkonev.examples.companies.VolumeTest#seven_thousand_companies" test
+```
+
+### Then create branch for every company three times (sequentially)
+```
+./mvnw -DenableLoadTests=true -Dtest="name.nkonev.examples.companies.VolumeTest#create_draft_for_each_company" test
+./mvnw -DenableLoadTests=true -Dtest="name.nkonev.examples.companies.VolumeTest#create_draft_for_each_company" test
+./mvnw -DenableLoadTests=true -Dtest="name.nkonev.examples.companies.VolumeTest#create_draft_for_each_company" test
+```
+
+### Then run every-second requests in three terminal windows in parallel
+```
+watch -n 1 curl -Ss --url 'http://localhost:8080/company'
+```
+
+```
+watch -n 1 curl -Ss --url 'http://localhost:8080/company'
+```
+
+```
+watch -n 1 curl -Ss --url 'http://localhost:8080/company'
+```
+
+Those requests turned into SQL like
+```sql
+CALL DOLT_CHECKOUT ('main');
+SELECT `company`.`name` AS `name`, `company`.`modified_at` AS `modified_at`, `company`.`id` AS `id`, `company`.`bank_account` AS `bank_account`, `company`.`estimated_size` AS `estimated_size` FROM `company` ORDER BY `company`.`name` ASC LIMIT 0, 20
+```
+
+(You can uncomment JDBC logging in `application.yaml`)
+
+Now we can see 20% CPU of dolt in `top`.
+
+I tried to create indexes
+```sql
+ALTER TABLE company
+    ADD INDEX company_name USING HASH (name);
+
+ALTER TABLE company
+    ADD INDEX company_name USING BTREE (name);
+```
+but it doesn't change the CPU load.
+
+
+### If we start creating branches again CPU of dolt will be 120%
+```
+./mvnw -DenableLoadTests=true -Dtest="name.nkonev.examples.companies.VolumeTest#create_draft_for_each_company" test
 ```
--- src/test/kotlin/name/nkonev/examples/companies/VolumeTest.kt
+++ src/test/kotlin/name/nkonev/examples/companies/VolumeTest.kt
@@ -1,10 +1,21 @@
 package name.nkonev.examples.companies
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.data.domain.PageRequest
+import org.springframework.http.HttpHeaders.CONTENT_TYPE
+import org.springframework.http.MediaType
+import org.springframework.http.RequestEntity
+import java.net.URI
+import java.time.Duration
+import java.time.Instant
 import java.util.*
 
 // This is separated test which works with database declared in application.yaml
@@ -13,17 +24,98 @@ import java.util.*
 class VolumeTest {
 
     @Autowired
-    lateinit var companiesController: CompaniesController // it is real java service, no http interaction during call
+    lateinit var companyRepository: CompanyRepository
 
     @Autowired
     lateinit var storageService: StorageService
 
+    @Autowired
+    lateinit var objectMapper: ObjectMapper
+
+    @Autowired
+    lateinit var companiesController: CompaniesController // it is real java service, no http interaction during call
+
     private val logger = LoggerFactory.getLogger(this::class.java)
+
+    val restTemplate = TestRestTemplate()
+
+    @Value("\${server.port:8080}")
+    lateinit var port: Integer
 
     val testUserId = UUID.randomUUID()
 
     @Test
-    fun fifty_thousands_branches() {
+    fun seven_thousand_companies() {
+
+        val timeBefore = measureTime {
+            val request = RequestEntity.get(URI.create("http://localhost:${port}/company"))
+                .build()
+            restTemplate.exchange(request, String::class.java)
+        }
+        logger.info("Request time before ${timeBefore}")
+
+        val insertionTime = measureTime {
+            storageService.checkoutBranch(MAIN_BRANCH)
+            for (i in 1..7000) {
+                val company = Company(UUID.randomUUID(), "Company number $i", new = true)
+                val savedCompany = companyRepository.save(company)
+                logger.info("Creating company $i")
+            }
+            storageService.addAndCommit(testUserId, "Save all the companies")
+        }
+        logger.info("Insertion time ${insertionTime}")
+
+        val timeAfter = measureTime {
+            val get = RequestEntity.get(URI.create("http://localhost:${port}/company"))
+                .build()
+            restTemplate.exchange(get, String::class.java)
+        }
+        logger.info("Request time after ${timeAfter}")
+    }
+
+    @Test
+    fun create_draft_for_each_company() {
+        storageService.checkoutBranch(MAIN_BRANCH)
+
+        val companies = companyRepository.findAll()
+
+        val measuredTime = measureTime {
+
+            var i = 0
+            val iter = companies.iterator()
+            // create the draft(branch) per every user
+            while(iter.hasNext()) {
+                i++
+                val company = iter.next()
+                logger.info("Writing ${i} company id=${company.identifier}")
+                val request1 = RequestEntity.post(URI.create("http://localhost:${port}/company/${company.identifier}/draft"))
+                    .header(USER_ID_HEADER, testUserId.toString())
+                    .build()
+                val response1 = restTemplate.exchange(request1, String::class.java)
+                logger.info("Create draft response: ${response1.statusCode}")
+
+                val responseObj1: CompaniesController.DraftResponse = objectMapper.readValue(response1.body!!)
+
+                val request2 = RequestEntity.put(URI.create("http://localhost:${port}/company/${company.identifier}/draft/${responseObj1.draftId}"))
+                    .header(USER_ID_HEADER, testUserId.toString())
+                    .header(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .body(objectMapper.writeValueAsString(CompaniesController.EditDraft("A change $i", company.copy(name = "Changed company $i"))))
+                val response2 = restTemplate.exchange(request2, String::class.java)
+                logger.info("Edit draft response: ${response2.statusCode}")
+            }
+        }
+        logger.info("Measured time ${measuredTime}")
+    }
+
+    private fun measureTime(runnable: Runnable): Duration {
+        val first = Instant.now()
+        runnable.run()
+        val second = Instant.now()
+        return Duration.between(first, second)
+    }
+
+    @Test
+    fun fifty_thousand_branches() {
         val company = Company(UUID.randomUUID(), "Company one", new = true)
         val savedCompany = companiesController.createCompany(company, testUserId)
         logger.info("Created company with name ${savedCompany.name}, id ${savedCompany.identifier}")
