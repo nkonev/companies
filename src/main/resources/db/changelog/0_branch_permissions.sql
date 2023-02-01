0c5c507f0f36f9824f428602b374dd432e677fa3
Nikita Konev <nikit.cpp@yandex.ru>
2023-02-01 23:18:03 +0400

    Use the new version of dolt with tree search

--- src/main/resources/db/changelog/0_branch_permissions.sql
+++
@@ -1,2 +0,0 @@
--- insert here the branch permissions to reduce new branch creation degradation https://github.com/dolthub/dolt/issues/5175#issuecomment-1402109985
-INSERT INTO dolt_branch_control VALUES ('%', '%', 'root', '%', 'admin');
